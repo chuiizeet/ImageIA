5a4ba487add593463f5415ea9e383d4e2d65bfaf
chuiizeet <chuy.guerra19@outlook.es>
2019-08-10 19:32:34 -0500

    Init files

---
+++ imageia/imageia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  imageia
+//
+//  Created by imac on 8/10/19.
+//  Copyright © 2019 JeguLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
