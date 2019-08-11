62ef4530fb4879db0790cf2d61344dd313b28cd5
chuiizeet <chuy.guerra19@outlook.es>
2019-08-11 00:52:33 -0500

    Erase background

--- imageia/imageia/Controller/HomeVC.swift
+++ imageia/imageia/Controller/HomeVC.swift
@@ -7,21 +7,57 @@
 //
 
 import UIKit
+import TCMask
 
-class HomeVC: UIViewController {
+class HomeVC: UIViewController, TCMaskViewDelegate {
     
     // MARK: - Properties
     
     
     // MARK: - Init
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        presentTCMaskView()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        setupViewComponents()
 
     }
     
     // MARK: - Helper Functions
     
+    func setupViewComponents() {
+        
+        
+    }
+    
+    func presentTCMaskView() {
+        let image = UIImage(named: "test2")!
+        let maskView = TCMaskView(image: image)
+        maskView.delegate = self
+        maskView.initialTool = .quickSelect
+        maskView.initialState = .subtract
+        
+        let viewMode = TCMaskViewMode.transparent()
+        maskView.viewModes = [viewMode]
+        
+
+        maskView.presentFrom(rootViewController: self, animated: true)
+    }
+    
+}
+
+extension UIImage {
+    func convertToGrayScaleNoAlpha() -> UIImage {
+        let colorSpace = CGColorSpaceCreateDeviceGray();
+        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.none.rawValue)
+        let context = CGContext(data: nil, width: Int(size.width), height: Int(size.height), bitsPerComponent: 8, bytesPerRow: 0, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)
+        context?.draw(self.cgImage!, in: CGRect(origin: CGPoint(), size: size))
+        return UIImage(cgImage: context!.makeImage()!)
+    }
 }
